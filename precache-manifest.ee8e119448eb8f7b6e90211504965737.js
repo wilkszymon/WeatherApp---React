5e46316c27e92354f634210768e69a42793186bd
Szymon Wilk <wilkszymon@yahoo.com>
2020-07-14 23:05:13 +0200

    Updates

---
+++ precache-manifest.ee8e119448eb8f7b6e90211504965737.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2f0e6e0f07a446203ac9f6474ef2a8",
+    "url": "/weatherApp---React/index.html"
+  },
+  {
+    "revision": "0c9cdc2311e6ac56906c",
+    "url": "/weatherApp---React/static/css/main.1b15547c.chunk.css"
+  },
+  {
+    "revision": "9c7f4e74551d3f30af39",
+    "url": "/weatherApp---React/static/js/2.c40ca06e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp---React/static/js/2.c40ca06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9cdc2311e6ac56906c",
+    "url": "/weatherApp---React/static/js/main.98c6e7ce.chunk.js"
+  },
+  {
+    "revision": "349e4c71565ec208a1b7",
+    "url": "/weatherApp---React/static/js/runtime-main.2fefec68.js"
+  },
+  {
+    "revision": "611645789efc3a4dafdd71add51df2c5",
+    "url": "/weatherApp---React/static/media/cloudy.61164578.svg"
+  },
+  {
+    "revision": "5ff3dbf1f3906048c30ce4be76f21844",
+    "url": "/weatherApp---React/static/media/rain.5ff3dbf1.svg"
+  },
+  {
+    "revision": "5fa9d007fd61254260ef4ec0f92445ff",
+    "url": "/weatherApp---React/static/media/snowing.5fa9d007.svg"
+  },
+  {
+    "revision": "c25afb20c27fe9e03d336cdccece1059",
+    "url": "/weatherApp---React/static/media/thermometer.c25afb20.svg"
+  }
+]);
